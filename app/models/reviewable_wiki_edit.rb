af6586cdc0dcc08bdd952e56eeab9c8c96e663ae
loid345 <volanar@tut.by>
2026-01-23 00:06:41 +0300

    Clean up client locale entries

---
+++ app/models/reviewable_wiki_edit.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+class ReviewableWikiEdit < Reviewable
+  def build_actions(actions, guardian, args)
+    return unless guardian.can_approve?(self)
+
+    actions.add(
+      :approve_wiki_edit,
+      icon: "check",
+      button_class: "btn-primary",
+      label: "js.reviewables.actions.approve_wiki_edit.title",
+    )
+    actions.add(
+      :reject_wiki_edit,
+      icon: "times",
+      button_class: "btn-danger",
+      label: "js.reviewables.actions.reject_wiki_edit.title",
+    )
+  end
+
+  def perform_approve_wiki_edit(performing_user, args)
+    pending = WikiModeration.pending_edit_for(target)
+    return create_result(:success, transition_to: :approved) unless pending
+
+    raw = args[:edited_raw].presence || pending["raw"]
+    apply_pending_edit!(target, raw, performing_user)
+    WikiModeration.clear_pending_edit!(target)
+
+    create_result(:success, transition_to: :approved)
+  end
+
+  def perform_reject_wiki_edit(performing_user, args)
+    WikiModeration.clear_pending_edit!(target)
+    create_result(:success, transition_to: :rejected)
+  end
+
+  def self.needs_review!(target:, created_by:, payload:)
+    existing =
+      ReviewableWikiEdit.pending.find_by(
+        target_type: target.class.name,
+        target_id: target.id,
+      )
+    return existing if existing
+
+    ReviewableWikiEdit.create!(
+      created_by: created_by,
+      target: target,
+      payload: payload,
+      status: Reviewable.statuses[:pending],
+    )
+  end
+
+  private
+
+  def apply_pending_edit!(post, raw, performing_user)
+    post.update!(
+      raw: raw,
+      last_editor_id: performing_user.id,
+      edit_reason: I18n.t("reviewable_wiki_edit.approved"),
+    )
+    post.rebake!
+  end
+end
--- plugin.rb
+++ plugin.rb
@@ -1,21 +1,69 @@
 # frozen_string_literal: true
 
-# name: discourse-plugin-name
-# about: TODO
-# meta_topic_id: TODO
-# version: 0.0.1
-# authors: Discourse
-# url: TODO
-# required_version: 2.7.0
+# name: discourse-wiki-moderation
+# about: Adds moderation for wiki edits with approve/reject flow.
+# version: 0.1.0
+# authors: Discourse Community
+# url: https://github.com/example/discourse-wiki-moderation
+# required_version: 3.2.0
 
-enabled_site_setting :plugin_name_enabled
+enabled_site_setting :wiki_moderation_enabled
 
-module ::MyPluginModule
-  PLUGIN_NAME = "discourse-plugin-name"
+module ::WikiModeration
+  PLUGIN_NAME = "discourse-wiki-moderation"
+  PENDING_EDIT_KEY = "wiki_moderation_pending_edit"
+
+  def self.pending_edit_for(post)
+    raw = post.custom_fields[PENDING_EDIT_KEY]
+    return if raw.blank?
+
+    JSON.parse(raw)
+  end
+
+  def self.store_pending_edit!(post, payload)
+    post.custom_fields[PENDING_EDIT_KEY] = payload.to_json
+    post.save_custom_fields
+  end
+
+  def self.clear_pending_edit!(post)
+    post.custom_fields.delete(PENDING_EDIT_KEY)
+    post.save_custom_fields
+  end
 end
 
-require_relative "lib/my_plugin_module/engine"
+require_relative "lib/wiki_moderation/engine"
+
+require_relative "app/models/reviewable_wiki_edit"
 
 after_initialize do
-  # Code which should run after Rails has finished booting
+  DiscourseEvent.on(:post_edited) do |post, editor|
+    next unless SiteSetting.wiki_moderation_enabled
+    next unless post.wiki
+    next if editor&.staff?
+    next if WikiModeration.pending_edit_for(post)
+
+    revision = post.revisions&.last
+    raw_change = revision&.modifications&.dig("raw")
+    next unless raw_change&.length == 2
+
+    old_raw, new_raw = raw_change
+    next if old_raw.blank? || new_raw.blank? || old_raw == new_raw
+
+    pending_payload = {
+      "raw" => new_raw,
+      "revision_id" => revision.id,
+      "edited_by_id" => editor&.id,
+      "created_at" => Time.zone.now.iso8601,
+    }
+
+    WikiModeration.store_pending_edit!(post, pending_payload)
+    post.update_columns(raw: old_raw)
+    post.rebake!
+
+    ReviewableWikiEdit.needs_review!(
+      target: post,
+      created_by: editor,
+      payload: pending_payload,
+    )
+  end
 end
