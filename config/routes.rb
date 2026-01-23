7fb3a7e8bd8c801dad75ca1bab122716bb47c139
loid345 <volanar@tut.by>
2026-01-23 10:53:06 +0300

    Guard wiki moderation routes against duplicates

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
 WikiModeration::Engine.routes.draw do
-  get "/" => "wiki_moderation#index", as: nil
+  get "/" => "wiki_moderation#index"
   post "/:id/approve" => "wiki_moderation#approve"
   post "/:id/reject" => "wiki_moderation#reject"
 end
 
 Discourse::Application.routes.append do
-  mount ::WikiModeration::Engine, at: "wiki-moderation", as: nil
-  get "/admin/plugins/wiki-moderation" => "admin/plugins#index", constraints: StaffConstraint.new
+  unless Discourse::Application.routes.named_routes.key?(:wiki_moderation)
+    mount ::WikiModeration::Engine, at: "wiki-moderation"
+    get "/admin/plugins/wiki-moderation" => "admin/plugins#index", constraints: StaffConstraint.new
+  end
 end
